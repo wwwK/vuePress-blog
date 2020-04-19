a55cdeb5a11979d7dc54b076f6479999cf7c9d8b
1327398885@qq.com <sungang@970316>
2020-04-19 23:09:05 +0800

    修改部分参数

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,20 +1,23 @@
-const headConfig=require("./config/headConfig");
-const nav=require("./config/navConfig");
-const sidebarConfig=require("./config/sidebarConfig");
+const headConfig = require("./config/headConfig");
+const nav = require("./config/navConfig");
+const sidebarConfig = require("./config/sidebarConfig");
+const pluginsConfig = require("./config/plugInsConfig");
 
 module.exports = {
     title: 'sunBT',
     description: '有道无术，术尚可求，有术无道，止于术',
-    dest: './dist',
+    // dest: './dist',
     port: '7000',
-    head:headConfig,
+    path:'/vuePress-blog/',
+    head: headConfig,
+    plugins: pluginsConfig,
     markdown: {
         lineNumbers: true
     },
     themeConfig: {
         logo: '/images/sun.png',
         nav: nav,
-        sidebar:sidebarConfig,
+        sidebar: sidebarConfig,
         sidebarDepth: 2,
         lastUpdated: '上次更新时间',
         searchMaxSuggestoins: 10,
